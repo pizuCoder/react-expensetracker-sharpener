352c93cf529f7edf0d65598565b35ac0b32b18e2
Priya Moghe <102720207+pizuCoder@users.noreply.github.com>
2023-04-15 16:57:58 +0530

    email verification works

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import AuthForm from "./Auth/AuthForm";
 import UpdateProfile from "./pages/updateProfile";
-
+import VerifyEmail from './Auth/VerifyEmail'
 function App() {
   
   return (
@@ -15,7 +15,9 @@ function App() {
           <h1>Welcome to Expense Tracker</h1>
           <a href="/profile">
             <button>go to profile page</button>
+            
           </a>
+          <VerifyEmail />
         </Route>
         <Route path="/profile">
           <UpdateProfile />
